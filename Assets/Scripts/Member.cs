edc297f12166d7aad0d44b2a3d6acba0009f19b0
정유빈 <yubeen.jeong@aiaracorp.com>
2018-10-27 06:34:22 +0900

    멤버 상태관리를 위한 자료구조 추가

---
+++ Assets/Scripts/Member.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Member{
+    private float hunger = 100;
+    private float sleepy = 100;
+    private float condition = 100;
+
+    public float Sleepy
+    {
+        get
+        {
+            return sleepy;
+        }
+
+        set
+        {
+            sleepy = Mathf.Clamp(value,0,100);
+        }
+    }
+
+    public float Condition
+    {
+        get
+        {
+            return condition;
+        }
+
+        set
+        {
+            condition = Mathf.Clamp(value, 0, 100);
+        }
+    }
+
+    public float Hunger
+    {
+        get
+        {
+            return hunger;
+        }
+
+        set
+        {
+            hunger = Mathf.Clamp(value, 0, 100);
+        }
+    }
+}
