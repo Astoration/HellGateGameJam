38fd977a1c1569e2f7ec6ffb26fa0cf3cbaa5a80
chlrkdqpffl <chlrkdqpffl@gmail.com>
2018-10-27 04:59:59 +0900

    - 초기 작업 커밋

---
+++ Assets/Scripts/SelectScene.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectScene : Singleton<SelectScene> {
+
+    public CardObject[] m_objCard;
+    public float        m_fFadeTime;
+    private int m_nInitCount = 0;
+
+    private void Awake()
+    {
+        m_objCard = new CardObject[3];
+    }
+
+    void Start () {
+        StartCoroutine(GameUtility.Instance.FadeIn(2.0f));
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(GameUtility.Instance.FadeIn(2.0f));
+        }
+
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            StartCoroutine(GameUtility.Instance.FadeOut(2.0f));
+        }
+    }
+
+    private void InitCards()
+    {
+        // json 불러오고 불러온 정보를 이용하여 데이터 세팅
+
+
+        foreach (var card in m_objCard)
+        {
+            int rand = Random.Range(0, 1);
+            if (0 == rand)
+            {
+                //card.Init(Gamedata.GameItem.eHot6);
+                card.Init(Gamedata.m_listGameItem[Random.Range(0, Gamedata.m_listGameItem.Count)]);
+            }
+            else
+            {
+                card.Init(Gamedata.m_listDuplicationItem[Random.Range(0, Gamedata.m_listDuplicationItem.Count)]);
+            }
+        }
+        m_nInitCount++;
+
+    }
+
+}
---
+++ Assets/Scripts/Utils/GameUtility.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUtility : Singleton<GameUtility> {
+
+    public Image    m_imgBlack;
+
+    public IEnumerator FadeIn(float fadeTime)
+    {
+        Color color = m_imgBlack.color;
+        float fTime = 1.0f;
+
+        color.a = Mathf.Lerp(0.0f, 1.0f, fTime);
+        m_imgBlack.gameObject.SetActive(true);
+
+        while (0.0f < color.a)
+        {
+            fTime -= Time.deltaTime / fadeTime;
+
+            color.a = Mathf.Lerp(0.0f, 1.0f, fTime);
+            m_imgBlack.color = color;
+
+            yield return null;
+        }
+    }
+
+
+    public IEnumerator FadeOut(float fadeTime)
+    {
+        Color color = m_imgBlack.color;
+        float fTime = 0.0f;
+
+        color.a = Mathf.Lerp(0.0f, 1.0f, fTime);
+        m_imgBlack.gameObject.SetActive(true);
+
+        while (color.a < 1.0f)
+        {
+
+            fTime += Time.deltaTime / fadeTime;
+
+            color.a = Mathf.Lerp(0.0f, 1.0f, fTime);
+            m_imgBlack.color = color;
+
+            yield return null;
+        }
+
+        // SceneManager.LoadScene(0);
+    }
+}
