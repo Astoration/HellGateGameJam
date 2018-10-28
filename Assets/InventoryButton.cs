3369246db99de6c9a61bec3dfe3b4a76f4d095b0
정유빈 <yubeen.jeong@aiaracorp.com>
2018-10-28 12:35:19 +0900

    인벤토리 추가

---
+++ Assets/InventoryButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryButton : MonoBehaviour {
+    private void OnMouseUpAsButton()
+    {
+        if (GameObject.Find("PopupSchedule") != null || GameObject.Find("EventPopup") != null) return;
+        PopupInventory.Instantiate().Init();
+    }
+}
